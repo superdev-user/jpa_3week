50ae555960d56cc2dd9ef0f755e4250f521edf2b
이시훈 <sihlee@smilegate.com>
2020-02-07 13:50:15 +0900

    setup

---
+++ src/main/java/com/superdev/jpa/JpaApplication.java
@@ -0,0 +1,33 @@
+package com.superdev.jpa;
+
+import javax.persistence.*;
+
+public class JpaApplication {
+
+	public static void main(String[] args) {
+
+		//엔티티 매니저 팩토리 생성
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+		EntityManager em = emf.createEntityManager(); //엔티티 매니저 생성
+
+		EntityTransaction tx = em.getTransaction(); //트랜잭션 기능 획득
+
+		try {
+
+
+			tx.begin(); //트랜잭션 시작
+
+
+			tx.commit();//트랜잭션 커밋
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback(); //트랜잭션 롤백
+		} finally {
+			em.close(); //엔티티 매니저 종료
+		}
+
+		emf.close(); //엔티티 매니저 팩토리 종료
+	}
+
+}
